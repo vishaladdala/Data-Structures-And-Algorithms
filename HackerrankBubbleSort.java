50686337cb00c319acfd3c0740b4a963a88f42fd
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-01-21 12:05:31 -0600

    added code

--- HackerrankBubbleSort.java
+++ HackerrankBubbleSort.java
@@ -35,3 +35,46 @@ First Element: 1
 Last Element: 3
 */
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        
+        int count = bubblesort(a,n);
+        System.out.println("Array is sorted in "+count+" swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+        
+    }  
+        
+        
+    static int bubblesort(int[] a,int n){
+        int z = 0;
+        for (int i = 0; i < n; i++) {
+     
+         for (int j = 0; j < n-i-1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            //swap(a[j], a[j + 1]);
+            int temp = a[j];
+          a[j] = a[j+1];
+          a[j+1] = temp;
+           z++;
+        }    
+            
+    }    
+    }
+        return z;
+    }
+}
