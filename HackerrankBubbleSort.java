fe8a6504555bcd88bb522a9544617967ab5683a2
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-01-21 11:59:54 -0600

    variable that keeps a running tally of all swaps that occur during execution of bubblesort

---
+++ HackerrankBubbleSort.java
@@ -0,0 +1,37 @@
+/*
+HackerrankBubbleSort.java
+Task 
+Given an -element array, , of distinct elements, sort array  in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+
+Array is sorted in numSwaps swaps., where  is the number of swaps that took place.
+First Element: firstElement, where  is the first element in the sorted array.
+Last Element: lastElement, where  is the last element in the sorted array.
+Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution.
+
+Input Format
+
+The first line contains an integer, , denoting the number of elements in array . 
+The second line contains  space-separated integers describing the respective values of .
+
+Constraints
+
+
+, 
+Output Format
+
+You must print the following three lines of output:
+
+Array is sorted in numSwaps swaps., where  is the number of swaps that took place.
+First Element: firstElement, where  is the first element in the sorted array.
+Last Element: lastElement, where  is the last element in the sorted array.
+Sample Input 0
+
+3
+1 2 3
+Sample Output 0
+
+Array is sorted in 0 swaps.
+First Element: 1
+Last Element: 3
+*/
+
