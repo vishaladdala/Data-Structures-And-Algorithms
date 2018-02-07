a1ba499c227d668ca47a8edea08ffdc05bfc2d5c
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-07 16:29:34 -0600

    Number of segemnts i==0ORcharati-1==spaceANDcharatinotspace

---
+++ StringSegments.java
@@ -0,0 +1,11 @@
+class StringSegments {
+    public int countSegments(String s) {
+        int count = 0;
+        for(int i = 0; i<s.length(); i++){
+            if((i==0 ||s.charAt(i-1)==' ') &&  s.charAt(i) != ' '){
+                count++;
+            }
+        }
+        return count;
+    }
+}
