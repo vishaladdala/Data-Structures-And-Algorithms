3b4b6d453c582c4f9addebb1b89c7ed4b295266d
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-09 23:32:20 -0600

    is the binary tree symmetric

---
+++ IsSymmetricBinaryTree.java
@@ -0,0 +1,42 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class IsSymmetricBinaryTree {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root,root);
+        
+    }
+    
+    public static boolean isMirror(TreeNode t1, TreeNode t2){
+        if(t1 == null && t2 == null){return true;}
+        
+        if(t1==null || t2 == null ){return false;}
+        
+        return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right,t2.left);
+    }
+}
