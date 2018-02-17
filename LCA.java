77f8f3975d8165a4bdffc9cf5495ba7bc2864496
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-16 20:26:47 -0600

    lowest common ancestor leetcode

---
+++ LCA.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){return root;}
+        
+        if(root.val > p.val && root.val < q.val){
+            return root;
+        }
+        
+        
+        if( root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        
+        if( root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
