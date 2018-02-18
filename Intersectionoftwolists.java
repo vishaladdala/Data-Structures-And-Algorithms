9b00eaebd67baef6b55b867228f3cc8d50c87f71
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-17 19:59:23 -0600

    using 2 ptrs

---
+++ Intersectionoftwolists.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+       if( headA == null || headB == null )
+			return null;
+		
+		ListNode curA = headA, curB = headB;
+		while( curA!=curB){
+			curA = curA==null?headB:curA.next;
+			curB = curB==null?headA:curB.next;
+		}
+		return curA;
+    }
+}
