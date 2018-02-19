f635d81093a07a03928080eccd24588880a92852
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-19 16:56:52 -0600

    return inde of largest which is atleast 2 times bigger than rest

---
+++ LargestAtleast2X.py
@@ -0,0 +1,51 @@
+'''
+747. Largest Number At Least Twice of Others
+DescriptionHintsSubmissionsDiscussSolution
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+
+Note:
+
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
+ '''
+
+class Solution:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max = 0
+        for num in nums:
+            if(num > max):
+                max = num
+                
+        
+        for num in nums:
+            if(max < 2*num and max!=num):
+                return -1
+            
+            
+        for i in range(0,len(nums)):
+            if nums[i] == max:
+                return i
+            
