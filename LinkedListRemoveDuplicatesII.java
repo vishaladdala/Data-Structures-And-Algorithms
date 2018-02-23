9d0a6dc799fc5fb2032bc981f74879c009bf5614
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-02-23 02:35:33 -0600

    Linked List medium leetcode finding non duplicates

---
+++ LinkedListRemoveDuplicatesII.java
@@ -0,0 +1,46 @@
+/*
+82. Remove Duplicates from Sorted List II
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+         if(head == null || head.next == null){
+             return head;
+         }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode prev = dummy;
+        ListNode cur = head;
+        
+        while(cur != null){
+            while( cur.next != null && cur.val == cur.next.val){
+                cur = cur.next;
+            }
+            
+            if(prev.next == cur){
+                prev = prev.next;
+            }
+            else{
+                prev.next = cur.next;
+            }
+            
+            cur = cur.next;
+        }
+        return dummy.next;
+     }
+ }
+
