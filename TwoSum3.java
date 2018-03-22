5b3031404ed399fda08bf5deebe36113a63705c0
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-03-22 00:31:29 -0500

    google question data structure design

---
+++ TwoSum3.java
@@ -0,0 +1,42 @@
+class TwoSum {
+     
+    HashMap<Integer,Integer> map ;
+    List<Integer> list ;
+    
+    
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<Integer,Integer>();
+        list = new ArrayList<Integer>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        
+         if (map.containsKey(number)) map.put(number, map.get(number) + 1);
+	    else {
+	        map.put(number, 1);
+	        list.add(number);
+	    }
+	
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+      
+           for (int i = 0; i < list.size(); i++){
+	        int num1 = list.get(i), num2 = value - num1;
+	        if ((num1 == num2 && map.get(num1) > 1) || (num1 != num2 && map.containsKey(num2))) return true;
+	    }
+	    return false;
+
+        
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
