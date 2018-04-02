a6b829adc10f75868320f7c7570432d0c29175ba
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-04-01 22:22:06 -0500

    using hashmap

---
+++ KeyboardRow.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String[] letterrow = {"QWERTYUIOP","ASDFGHJKL","ZXCVBNM"};
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < letterrow.length; i++){
+            
+            for( char c : letterrow[i].toCharArray()){
+                map.put(c,i);
+            }
+            
+        }
+        
+        List<String> res = new ArrayList<>();
+        
+        
+        for( String w : words){
+            if( w.equals("")){
+                continue;
+            }
+            
+            int index = map.get(w.toUpperCase().charAt(0));
+            
+            for( char c : w.toUpperCase().toCharArray()){
+                if(map.get(c)!= index){
+                    index = -1;
+                    break;
+                }
+            }
+            
+            if(index != - 1){res.add(w);}
+        }
+        
+        return res.toArray(new String[0]);
+        
+    }
+}
