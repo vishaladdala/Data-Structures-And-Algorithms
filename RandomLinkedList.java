7f74582383a643781e02a7ee6b9d749d9d113c47
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-05-29 00:33:16 -0500

    copy a list with random pointer using hashmap

---
+++ RandomLinkedList.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null){
+            return null;
+        }
+        
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+        
+        //Step 1: copying all the nodes
+        RandomListNode node = head;
+        
+        while(node != null){
+            map.put( node, new RandomListNode(node.label));
+            node = node.next;
+        }
+        
+        //Step 2: copying the pointers
+        node = head;
+        
+        while(node!=null){
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            
+            node = node.next;
+        }
+        
+        return map.get(head);
+    }
+}
